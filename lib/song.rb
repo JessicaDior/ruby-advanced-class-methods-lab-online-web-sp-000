ce8fa189823597714640dfb180d9f644e26c784b
Jc Dior <jessicaclairdior@gmail.com>
2020-09-18 02:20:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,15 +2,15 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
-def self.create
-end
-
   def self.all
     @@all
   end
 
   def save
     self.class.all << self
+  end
+
+  def self.create
   end
 
 

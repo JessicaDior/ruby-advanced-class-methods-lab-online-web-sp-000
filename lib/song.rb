44cfa1468d801299f65b4a6f591ceb11885e9bf5
Jc Dior <jessicaclairdior@gmail.com>
2020-09-18 03:06:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,7 +43,7 @@ end
 
 def self.new_from_filename(filename)
   split_filename = filename.chomp(".mp3").split(" - ")
-  song = song.new 
+  song = song.new
   song.name = split_filename[1]
   song.artist_name = split_filename[0]
   song
